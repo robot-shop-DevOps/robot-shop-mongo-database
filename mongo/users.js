1eaf42f92caf3b45383e0f99a64d9ebd1ada0cf6
Shashank Palla <pallasai_shashank@epam.com>
2025-11-12 12:19:30 +0530

    updated mongo scripts to follow idempotency

--- mongo/users.js
+++ mongo/users.js
@@ -1,10 +1,22 @@
 db = db.getSiblingDB('users');
-db.users.insertMany([
-    {name: 'dummy', password: 'dummy', email: 'dummy@me.com'},
-]);
 
-// unique index on the name
-db.users.createIndex(
-    {name: 1},
-    {unique: true}
-);
+const existingIndexes = db.users.getIndexes().map(i => i.name);
+if (!existingIndexes.includes('name_1')) {
+  db.users.createIndex({ name: 1 }, { unique: true });
+  print('Created unique index on "name"');
+} else {
+  print('Unique index on "name" already exists, skipping.');
+}
+
+const defaultUsers = [
+  { name: 'dummy', password: 'dummy', email: 'dummy@me.com' },
+];
+
+defaultUsers.forEach(user => {
+  if (!db.users.findOne({ name: user.name })) {
+    db.users.insertOne(user);
+    print('Created user: ${user.name}');
+  } else {
+    print('User ${user.name} already exists, skipping.');
+  }
+});
